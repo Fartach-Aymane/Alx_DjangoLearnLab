357b105e54290b6ae9504313b4f1b715a7fd6ac1
raja <rajaemoutaouakil362@gmail.com>
2025-11-04 17:35:44 +0100

    Final URLs including book/library views and authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import list_books  # Explicit import required by ALX
-from .views import LibraryDetailView  # Explicit import for CBV
+from .views import list_books, LibraryDetailView
+from .views import register_view, login_view, logout_view  # explicit imports required by ALX
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # Function-Based View
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-Based View
+    # Function-Based View: list all books
+    path('books/', list_books, name='list_books'),
+
+    # Class-Based View: library details
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
